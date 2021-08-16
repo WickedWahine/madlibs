d6d3eb5e86e1da178d9b306f6b6df32d8fe7470a
Sarah Patanroi <sarahpatanroi@gmail.com>
2021-08-16 08:28:34 +0000

    Adding a few more .html

--- madlibs.py
+++ madlibs.py
@@ -41,6 +41,15 @@ def greet_person():
                            person=player,
                            compliment=compliment)
 
+@app.route('/game')
+def show_madlib_form():
+    """If person wants play"""
+
+    if play_game == 'no':
+        return render_template("goodbye.html")
+    elif:
+        return render_template("game.html")
+
 
 if __name__ == '__main__':
     # Setting debug=True gives us error messages in the browser and also
